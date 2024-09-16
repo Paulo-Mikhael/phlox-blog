9ba298893d178f297aa13cf9f05856b29d005624
Paulo-Mikhael <paulomiguel11111971@gmail.com>
2024-09-16 17:29:42 -0400

    Refazendo personalização de perfilç

--- src/components/PersonalizePerfilModal/index.tsx
+++ src/components/PersonalizePerfilModal/index.tsx
@@ -1,35 +1,14 @@
-import { ArrowRightLeft, Pencil, Trash } from "lucide-react";
-import { colors } from "../../styles/variables";
-import { Button } from "../Button";
-import { useNavigate } from "react-router-dom";
 import { useRecoilValue } from "recoil";
 import { actualUserState } from "../../state/atom";
-import { deleteUser, User } from "firebase/auth";
-import { useSetModalValue } from "../../state/hooks/useSetModalValue";
-import { ChangeEmailModal } from "../ChangeEmailModal";
-import { useState } from "react";
 import { Modal } from "../Modal";
 
 export function PersonalizePerfilModal() {
-  const navigate = useNavigate();
   const actualUser = useRecoilValue(actualUserState);
-  const setOpenModalCEM = useSetModalValue("CEM");
-  const setOpenModalPPM = useSetModalValue("PPM");
-  const [password, setPassword] = useState<string>("");
 
   if (!actualUser) return;
 
-  function Delete(user: User) {
-    deleteUser(user).then(() => {
-      navigate("/", { replace: true });
-    }).catch((error) => {
-      console.log(error);
-    });
-  }
-
   return (
     <>
-      <ChangeEmailModal password={password} setPassword={setPassword} onContinue={() => actualUser.auth && Delete(actualUser.auth)} />
       <Modal modalKey="PPM">
         <figure className="w-full flex flex-col justify-center items-center gap-3">
           <img
@@ -39,35 +18,8 @@ export function PersonalizePerfilModal() {
           />
           <figcaption className="text-typo-700 text-section-subtitle font-bold flex gap-2 items-center">
             {actualUser.data.email}
-            <Pencil
-              color={colors.typo[700]}
-              size={18}
-              className="cursor-pointer"
-              onClick={() => {
-                setOpenModalPPM(false);
-                setOpenModalCEM(true);
-              }}
-            />
           </figcaption>
         </figure>
-        <div className="mt-4 flex flex-col gap-2">
-          <Button.Root
-            variant="outlined"
-            onClick={() => navigate("/login")}
-          >
-            <Button.Text content="Trocar de Conta" />
-            <Button.Icon icon={ArrowRightLeft} />
-          </Button.Root>
-          <Button.Root
-            onClick={() => {
-              console.log(actualUser.auth?.email);
-              actualUser.auth && Delete(actualUser.auth);
-            }}
-          >
-            <Button.Text content="Excluir Minha Conta" />
-            <Button.Icon icon={Trash} />
-          </Button.Root>
-        </div>
       </Modal>
     </>
   );
