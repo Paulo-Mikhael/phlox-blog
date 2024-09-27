f769db7ed68c9097b6cf6ae0656ace5951436f19
Paulo-Mikhael <paulomiguel11111971@gmail.com>
2024-09-27 08:37:52 -0400

    consertando upload de imagem

--- src/utils/firebase/functions/submitImageToStorage.ts
+++ src/utils/firebase/functions/submitImageToStorage.ts
@@ -1,8 +1,11 @@
 import { getUrlFromStorage } from "./getUrlFromStorage";
 import { insertToStorage } from "./insertToStorage";
+import { v4 as uuidV4 } from "uuid";
 
 export function submitImageToStorage(file: File): Promise<string> {
-  const path = `images/${file.name.trim()}`;
+  const imageName = file.name.trim();
+  const randomName = uuidV4();
+  const path = `images/${imageName === "image.png" ? randomName : imageName}`;
 
   return new Promise((resolve, reject) => {
     insertToStorage(path, file)
